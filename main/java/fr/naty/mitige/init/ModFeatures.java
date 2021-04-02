84c2467a80538b2dd355d6b546d8c01f94314b94
ZukeDST <65614804+ZukeDST@users.noreply.github.com>
2021-04-02 19:11:02 +0200

    Update ModFeatures.java

--- main/java/fr/naty/mitige/init/ModFeatures.java
+++ main/java/fr/naty/mitige/init/ModFeatures.java
@@ -20,7 +20,7 @@ public class ModFeatures {
     //on fait appel a ModOreFeature au lieu de OreFeature, puis on utilise la regle etablie dans cette classe (ici END)
     public void init()
     {
-        ORE_BESKAR_FEATURE = register("ore_beskar", Feature.ORE.withConfiguration(new OreFeatureConfig(ModOreFeatureConfig.FillerBlockType.END, MitigeBlocks.Beskar_ore.get().getDefaultState(), 3))
+        ORE_BESKAR_FEATURE = register("ore_beskar", Feature.ORE.withConfiguration(new OreFeatureConfig(ModOreFeatureConfig.FillerBlockType.END_STONE, MitigeBlocks.Beskar_ore.get().getDefaultState(), 3))
         .square()
         .range(40)
         .func_242731_b(4));
